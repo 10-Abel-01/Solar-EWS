3404035258e13ac23ec16ef7c252dc831dbb3a74
10-Abel-01 <abelsaferyan06@gmail.com>
2026-04-22 13:32:27 +0700

    change file

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "./components/MainLayout";
+import MonitorLayout from "./components/layout/MonitorLayout";
 import Dashboard from "./pages/Dashboard";
 import Analytics from "./pages/Analytics";
 import Diagnostics from "./pages/Diagnostics";
 import Documentation from "./pages/Documentation";
-import MonitorLayout from "./components/layout/MonitorLayout";
 
 import "./App.css";
 import "./index.css";
